09debf9abcd193d736b297ed729715081ed6f871
Otavio Celestino dos Santos <hunter@pop-os.localdomain>
2022-09-07 20:13:04 -0300

    Adicionando benchmarks em Go

---
+++ benchmarks/primeNumbers_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+var num = 100
+
+var table = []struct {
+	input int
+}{
+	{input: 100},
+	{input: 1000},
+	{input: 74382},
+	{input: 382399},
+}
+
+func BenchmarkPrimeNumbers(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		primeNumbers(num)
+	}
+}
+
+func BenchmarkPrimeNumbersImproved(b *testing.B) {
+	for _, v := range table {
+		b.Run(fmt.Sprintf("input_size_%d", v.input), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				isPrimeImproved(v.input)
+			}
+		})
+	}
+}
